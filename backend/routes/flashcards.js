10e429be9472e19ba25882f458b821e0c39c0959
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2024-01-03 00:12:36 -0700

    fixed getSingle

--- backend/routes/flashcards.js
+++ backend/routes/flashcards.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const flashcardController = require('../controllers/flashcards');
 
 router.get('/', flashcardController.getAll);
-router.get('/id', flashcardController.getSingle);
+router.get('/:id', flashcardController.getSingle);
 router.post('/', flashcardController.createCard);
 router.put('/:id', flashcardController.updateCard);
 router.delete('/:id', flashcardController.deleteCard);
